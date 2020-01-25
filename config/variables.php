46ee6455491fe0fa118a8fe22092525e8e54769f
Mandani, Mark Vincent G <mark.mandani@summitmedia.com.ph>
2020-01-25 21:02:21 +0800

    manage users

--- config/variables.php
+++ config/variables.php
@@ -2,7 +2,7 @@
 DEFINE('DB_NAME','exam_mandani');
 DEFINE('DB_HOST','localhost');
 DEFINE('DB_USERNAME','root');
-DEFINE('DB_PASSWORD','');
+DEFINE('DB_PASSWORD','root123');
 DEFINE('DB_PREFIX','tbl_');
 
 DEFINE('TPL_PATH', ROOT_PATH."/templates");
